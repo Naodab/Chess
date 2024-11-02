e70bbef7089bdcb67a4875e5bfc3036e0f8f1006
Naodab <nguyenhobadoan110304@gmail.com>
2024-11-02 13:06:34 +0700

    add service upload file for setting avatar

---
+++ src/main/java/com/example/pbl4Version1/controller/FileUploadController.java
@@ -0,0 +1,29 @@
+package com.example.pbl4Version1.controller;
+
+import com.example.pbl4Version1.dto.response.ApiResponse;
+import com.example.pbl4Version1.service.CloudinaryService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/avatar")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class FileUploadController {
+    CloudinaryService cloudinaryService;
+
+    @PostMapping
+    public ApiResponse<String> setAvatar(@RequestParam("file") MultipartFile file)
+            throws IOException {
+        String url = cloudinaryService.uploadFile(file);
+        return ApiResponse.<String>builder().result(url).build();
+    }
+}
