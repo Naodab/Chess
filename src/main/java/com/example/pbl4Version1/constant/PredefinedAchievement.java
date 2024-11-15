b7c766f45442cfb78fbe9bc450ab4cae257071a2
binhbo73 <phuongbinh732004@gmail.com>
2024-11-16 00:16:31 +0700

    fix_page_home

--- src/main/java/com/example/pbl4Version1/constant/PredefinedAchievement.java
+++ src/main/java/com/example/pbl4Version1/constant/PredefinedAchievement.java
@@ -4,87 +4,111 @@ import com.example.pbl4Version1.entity.Achievement;
 
 public class PredefinedAchievement {
 
-	
 
-	// Achievement format: new Achievement(title, imageUrl, description)
 
 	// 1. Welcome
-	private Achievement welcome = new Achievement("Welcome", "../assets/img/achievement/welcome.png", "Create your chess account");
+	private Achievement welcome 
+	= new Achievement("Chào mừng", "../assets/img/achievement/welcome.png", "Tạo tài khoản cờ vua của bạn");
 
 	// 2. Self-portrait
-	private Achievement selfPortrait = new Achievement("Self-portrait", "../assets/img/achievement/selfPortrait.png", "Change your avatar from the default");
+	private Achievement selfPortrait 
+	= new Achievement("Tự chụp ảnh", "../assets/img/achievement/selfPortrait.png", "Thay đổi ảnh đại diện của bạn từ mặc định");
 
 	// 3. Live for the first time!
-	private Achievement liveFirstTime = new Achievement("Live for the first time!", "../assets/img/achievement/liveFirstTime.png", "Play a live game");
+	private Achievement liveFirstTime 
+	= new Achievement("Trực tiếp lần đầu tiên!", "../assets/img/achievement/liveFirstTime.png", "Chơi một ván trực tiếp");
 
 	// 4. Daily first!
-	private Achievement dailyFirst = new Achievement("Daily first!", "../assets/img/achievement/dailyFirst.png", "Play Games Daily");
+	private Achievement dailyFirst 
+	= new Achievement("Hàng ngày đầu tiên!", "../assets/img/achievement/dailyFirst.png", "Chơi trò chơi hàng ngày");
 
 	// 5. First Word
-	private Achievement firstWord = new Achievement("First Word", "../assets/img/achievement/firstWord.png", "Comment or Send Message");
+	private Achievement firstWord 
+	= new Achievement("Từ đầu tiên", "../assets/img/achievement/firstWord.png", "Bình luận hoặc gửi tin nhắn");
 
 	// 6. Human vs Machine
-	private Achievement humanVsMachine = new Achievement("Human vs Machine", "../assets/img/achievement/humanVsMachine.png", "Play a Live Chess Game Against a Computer");
+	private Achievement humanVsMachine 
+	= new Achievement("Con người đấu với máy", "../assets/img/achievement/humanVsMachine.png", "Chơi một ván cờ vua trực tiếp với máy tính");
 
 	// 7. Live 10
-	private Achievement live10 = new Achievement("Live 10", "../assets/img/achievement/live10.png", "Play 10 Live Games");
+	private Achievement live10 
+	= new Achievement("Trực tiếp 10", "../assets/img/achievement/live10.png", "Chơi 10 ván trực tiếp");
 
 	// 8. Live 100
-	private Achievement live100 = new Achievement("Live 100", "../assets/img/achievement/live100.png", "Play 100 Live Games");
+	private Achievement live100 
+	= new Achievement("Trực tiếp 100", "../assets/img/achievement/live100.png", "Chơi 100 ván trực tiếp");
 
 	// 9. Daily 5
-	private Achievement daily5 = new Achievement("Daily 5", "../assets/img/achievement/daily5.png", "Play 5 Daily Games");
+	private Achievement daily5 
+	= new Achievement("Hàng ngày 5", "../assets/img/achievement/daily5.png", "Chơi 5 ván hàng ngày");
 
 	// 10. Daily 10
-	private Achievement daily10 = new Achievement("Daily 10", "../assets/img/achievement/daily10.png", "Play 10 Daily Games");
+	private Achievement daily10 
+	= new Achievement("Hàng ngày 10", "../assets/img/achievement/daily10.png", "Chơi 10 ván hàng ngày");
 
 	// 11. Assassin Pawn
-	private Achievement assassinPawn = new Achievement("Assassin Pawn", "../assets/img/achievement/assassinPawn.png", "Checkmate with a pawn (promoted pieces also work)");
+	private Achievement assassinPawn 
+	= new Achievement("Tốt sát thủ", "../assets/img/achievement/assassinPawn.png", "Chiếu hết bằng tốt ");
 
 	// 12. Assassin Knight
-	private Achievement assassinKnight = new Achievement("Assassin Knight", "../assets/img/achievement/assassinKnight.png", "Checkmate with a knight");
+	private Achievement assassinKnight 
+	= new Achievement("Mã sát thủ", "../assets/img/achievement/assassinKnight.png", "Chiếu hết bằng mã");
 
 	// 13. Assassin Bishop
-	private Achievement assassinBishop = new Achievement("Assassin Bishop", "../assets/img/achievement/assassinBishop.png", "Checkmate with a bishop");
+	private Achievement assassinBishop 
+	= new Achievement("Sát thủ Tượng", "../assets/img/achievement/assassinBishop.png", "Chiếu hết với Tượng");
 
 	// 14. Killer Rook
-	private Achievement killerRook = new Achievement("Killer Rook", "../assets/img/achievement/killerRook.png", "Checkmate with a rook");
+	private Achievement killerRook 
+	= new Achievement("Xe Sát Thủ", "../assets/img/achievement/killerRook.png", "Chiếu hết với Xe");
 
 	// 15. Assassin Queen
-	private Achievement assassinQueen = new Achievement("Assassin Queen", "../assets/img/achievement/assassinQueen.png", "Checkmate with a queen");
+	private Achievement assassinQueen 
+	= new Achievement("Hậu Sát Thủ", "../assets/img/achievement/assassinQueen.png", "Chiếu hết với Hậu");
 
 	// 16. Castle Conqueror
-	private Achievement castleConqueror = new Achievement("Castle Conqueror", "../assets/img/achievement/castleConqueror.png", "Checkmate by castling");
+	private Achievement castleConqueror 
+	= new Achievement("Chinh Phục Lâu Đài ", "../assets/img/achievement/castleConqueror.png", "Chiếu hết bằng cách nhập thành");
 
 	// 17. World Traveler
-	private Achievement worldTraveler = new Achievement("World Traveler", "../assets/img/achievement/worldTraveler.png", "Play with people from 10 different countries (except your own)");
+	private Achievement worldTraveler 
+	= new Achievement("Du Hành Thế Giới", "../assets/img/achievement/worldTraveler.png", "Chơi với những người từ 10 quốc gia khác nhau");
 
 	// 18. Chess Marathon
-	private Achievement chessMarathon = new Achievement("Chess Marathon", "../assets/img/achievement/chessMarathon.png", "Play a game with over 100 moves");
+	private Achievement chessMarathon 
+	= new Achievement("Marathon Cờ Vua", "../assets/img/achievement/chessMarathon.png", "Chơi một ván cờ với hơn 100 nước đi");
 
 	// 19. Rapid Strike
-	private Achievement rapidStrike = new Achievement("Rapid Strike", "../assets/img/achievement/rapidStrike.png", "Play a game with less than 20 moves");
+	private Achievement rapidStrike 
+	= new Achievement("Đòn Nhanh", "../assets/img/achievement/rapidStrike.png", "Chơi một ván cờ với ít hơn 20 nước đi");
 
 	// 20. Nick of Time
-	private Achievement nickOfTime = new Achievement("Nick of Time", "../assets/img/achievement/nickOfTime.png", "Win a game with less than a second remaining");
+	private Achievement nickOfTime 
+	= new Achievement("Thời gian gấp rút", "../assets/img/achievement/nickOfTime.png", "Thắng một ván cờ khi còn chưa đầy một giây");
 
 	// 21. Full House
-	private Achievement fullHouse = new Achievement("Full House", "../assets/img/achievement/fullHouse.png", "Win a game without losing a piece");
+	private Achievement fullHouse 
+	= new Achievement("Nhà đầy đủ", "../assets/img/achievement/fullHouse.png", "Thắng một ván cờ mà không mất một quân nào");
 
 	// 22. Ballot Box
-	private Achievement ballotBox = new Achievement("Ballot Box", "../assets/img/achievement/ballotBox.png", "Vote in a Voting Game");
+	private Achievement ballotBox 
+	= new Achievement("Hộp Phiếu", "../assets/img/achievement/ballotBox.png", "Bỏ phiếu trong một ván bỏ phiếu");
 
 	// 23. Say Hello
-	private Achievement sayHello = new Achievement("Say Hello", "../assets/img/achievement/sayHello.png", "Send a Message");
+	private Achievement sayHello 
+	= new Achievement(" Nói Xin Chào", "../assets/img/achievement/sayHello.png", "Gửi Tin Nhắn");
 
 	// 24. Devoted
-	private Achievement devoted = new Achievement("Devoted", "../assets/img/achievement/devoted.png", "Log in Every Day for 7 Days");
+	private Achievement devoted 
+	= new Achievement("Tận Tâm", "../assets/img/achievement/devoted.png", "Đăng Nhập Mỗi Ngày Trong 7 Ngày");
 
 	// 25. Fanatic
-	private Achievement fanatic = new Achievement("Fanatic", "../assets/img/achievement/fanatic.png", "Log in Every Day for 30 Days");
+	private Achievement fanatic 
+	= new Achievement("Cuồng Nhiệt", "../assets/img/achievement/fanatic.png", "Đăng Nhập Mỗi Ngày Trong 30 Ngày");
 
 	// 26. Finger Paint
-	private Achievement fingerPaint = new Achievement("Finger Paint", "../assets/img/achievement/fingerPaint.png", "Customize Your Theme");
+	private Achievement fingerPaint 
+	= new Achievement("Sơn Ngón Tay", "../assets/img/achievement/fingerPaint.png", "Tùy Chỉnh Chủ Đề Của Bạn");
 
 	
 
